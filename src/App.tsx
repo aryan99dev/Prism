027ed889b627007b42e624fa88086cd452f692d2
Aryan Sharma <aryansharma06127@gmail.com>
2025-07-07 10:22:21 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 
-import './globals.css';
-import Home from "./_root/pages/Home.tsx";
 import SigninForm from "./_auth/forms/SigninForm.tsx";
+import SignupForm from "./_auth/forms/SignupForm.tsx";
+import AuthLayout from "./_auth/AuthLayout.tsx";
+import RootLayout from "./_root/RootLayout.tsx";
+import Home from "./_root/pages/Home.tsx";
+import './globals.css';
+
 
 const App = () => {
     return (
         <main className="flex h-screen">
-            <Routes>
-            {/*   Public Routes */}
-                <Route  path="/sign-in" element={<SigninForm />}/>
-            {/*   Private Routes */}
+            <Routes >
+                {/*   Public Routes */}
+                <Route element={<AuthLayout />} >
+                    <Route  path="/sign-in" element={<SigninForm />}/>
+                    <Route  path="/sign-up" element={<SignupForm />}/>
+                </Route>
+                {/*   Private Routes */}
+                <Route element={<RootLayout />}>
                     <Route index element={<Home />}/>
+                </Route>
             </Routes>
 
         </main>
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button.tsx";
+
+const SignupForm = () => {
+    return (
+        <div>
+        <Button>Click</Button>
+        </div>
+    )
+}
+export default SignupForm
